cf56993dc9f175b91e5248d6a82d5ba2fe01a228
Edilson Londoño Castañeda <eddylson.londonio@gmail.com>
2019-09-21 02:27:34 -0500

    Login controllers

--- Server/controllers/login.js
+++ Server/controllers/login.js
@@ -1,6 +1,8 @@
 const jwt = require('jwt-simple'); 
 const {connection} = require('../Database/DB');
 const bcrypt = require('bcrypt');
+const Keys = require('../config/secret');
+
 let login = (req,res) => {
     let {email , password} = req.body;
     connection.query('SELECT Nombre_usuario , Correo , contrasenia FROM Usuarios WHERE Correo = ? ',[email], (err,result) => {
@@ -9,7 +11,7 @@ let login = (req,res) => {
         bcrypt.compare(password,result[0].contrasenia,(err,resp)=> {
             if (err) return res.status(500).send({message : `Error : ${err}`});
             if (!resp) return res.status(403).send({mesage : ' Acceso denegado'});
-            let token = jwt.encode(result,'12345');
+            let token = jwt.encode(result,Keys.secret);
             return res.status(200).send({message : token});
         });
         
