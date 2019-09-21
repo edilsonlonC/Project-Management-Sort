2d6a014c5f806137591e8ae262eadd9af72fdc4b
Edilson Londoño Castañeda <eddylson.londonio@gmail.com>
2019-09-21 02:28:16 -0500

    Authentication JWT

--- Server/routes/user.js
+++ Server/routes/user.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const controllerUser = require('../controllers/user');
+const Auth = require('../middleware/Auth');
+
+// Routes 
 router.get('/', controllerUser.home);
 router.post('/save-user', controllerUser.saveuser);
-router.get('/list-users',controllerUser.listUsers);
+router.get('/list-users',Auth.isAuth,controllerUser.listUsers);
 router.get('/list-users/:id',controllerUser.listUser);
 router.put('/update-user/:id',controllerUser.updateUser);
 
