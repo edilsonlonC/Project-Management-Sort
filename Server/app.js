5e0531801dec281334240fbba70ba07edb8c73ea
Edilson Londoño Castañeda <eddylson.londonio@gmail.com>
2019-09-20 20:43:32 -0500

    server config

---
+++ Server/app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port , (err) => {
+    if (err) return console.log(err);
+    console.log(`server is http://localhost:${port}`);
+})
