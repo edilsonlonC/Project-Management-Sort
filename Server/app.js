9686dba470db3921c4bec1b9e7fbe1e079a688a6
Edilson Londoño Castañeda <eddylson.londonio@gmail.com>
2019-09-21 01:27:37 -0500

    Login config

--- Server/app.js
+++ Server/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express();
-const {router} = require('./routes/user'); 
+const {router} = require('./routes/user');
+const {routerLogin}= require('./routes/login');
 const port = process.env.PORT || 3000;
 //middelware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use('/api',router);
+app.use('/api',routerLogin);
 
 // server started
 app.listen(port , (err) => {
