63c64c38a60d6602a698e75d4260d121feee867c
Edilson Londoño Castañeda <eddylson.londonio@gmail.com>
2019-09-21 20:07:08 -0500

    Projects Saved

--- Server/controllers/project.js
+++ Server/controllers/project.js
@@ -1,10 +1,37 @@
 const {connection} = require('../Database/DB');
 
+
+
+let saveProject = (req,res) => {
+    let {projectName} = req.body;
+    if (!projectName) return res.status(404).send({message : 'parametros invalidos'});
+    connection.query('SELECT Nombre_Proyecto FROM Proyecto WHERE Nombre_Proyecto = ? ' ,[projectName], (err,result)=> {  
+        if (err) return res.status(500).send({ err});
+        if (result.length >= 1) return res.status(404).send({message : `El proyecto ${projectName} ya existe`});
+        connection.query('INSERT INTO Proyecto (Nombre_Proyecto) values(?)',[projectName],(err2,result2)=>{
+            if (err) return res.status(500).send({message : err2})
+            return res.status(200).send({message : result2});
+        })
+        
+    })
+    
+}
 let projects = (req,res) => {
     res.status(200).send({message : 'projects here '});
 }
 
+let updateProject = (req,res) => {
+    res.status(200).send({message : 'update project'});
+}
+
+let deleteProject = (req,res) => {
+    res.status(200).send({message : 'delete project'});
+}
+
 
 module.exports = {
-    projects
+    projects,
+    saveProject,
+    updateProject,
+    deleteProject
 }
--- Server/routes/project.js
+++ Server/routes/project.js
@@ -2,6 +2,9 @@ const routerProject = require('express').Router();
 const controllesProjects = require('../controllers/project');
 
 routerProject.get('/projects',controllesProjects.projects);
+routerProject.post('/save-project',controllesProjects.saveProject);
+routerProject.put('/update-project',controllesProjects.updateProject);
+routerProject.delete('/delete-project',controllesProjects.deleteProject);
 
 module.exports = {
     routerProject
