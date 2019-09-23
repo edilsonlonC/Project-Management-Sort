f977ac5f124f1f52d7c980a6516a7ccd962f46b2
Edilson Londoño Castañeda <eddylson.londonio@gmail.com>
2019-09-23 11:58:44 -0500

    Types controllers

---
+++ Server/controllers/project-types.js
@@ -0,0 +1,15 @@
+const {connection} = require('../Database/DB');
+
+
+let projectsTypes = (req,res) =>  {
+    connection.query('SELECT * FROM Tipo_Proyecto',(err,result) => {
+        if (err) return res.status(500).send({message : `error ${err}`});
+        if (result.length < 1 ) return res,status(404).send({message : 'No hay tipos en la base de datos '});
+        return res.status(200).send({types : result});
+    })
+   
+}
+
+module.exports = {
+    projectsTypes
+}
