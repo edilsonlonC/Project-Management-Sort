d092424797ae13f84f6f10324f8c515fbb71253a
Edilson Londoño Castañeda <eddylson.londonio@gmail.com>
2019-09-28 14:58:13 -0500

    Headers

--- Server/middleware/Auth.js
+++ Server/middleware/Auth.js
@@ -3,13 +3,18 @@ const key = require('../config/secret');
 /* "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.W3siTm9tYnJlX3VzdWFyaW8iOiJFZGlsc29uIiwiQ29ycmVvIjoiZWRkeWxzb24ubG9uZG9uaW9AZ21haWwuY29tIiwiY29udHJhc2VuaWEiOiIkMmIkMTAkVE4yQzZMRm9UTTR0MlBiUTNkYWc0dTkvM0hWcGdXZFhITzFQR2g3YXROeU5OZk5ydlcvLnUifV0.ky_JQodF1TndBowTvmsgpF43s4cBK9WWRi_YM2Dsds0" */
 let isAuth = (req,res,next)=> {
     
-    let {Authorization} = req.body;
-    if (Authorization) {
-        Authorization = Authorization.split(' ');
-        let token = Authorization[1];
-        console.log('here token : ',token);
+    let {authorization} = req.headers;
+    console.log('headers ' ,req.headers);
+  //  return res.status(404).send({authorization})
+    if (authorization) {
+         console.log('here');
+         
+         authorization = authorization.split(' ');
+        let token = authorization[1];
+        console.log(token);
+        
         try {
-            let decoded = jwt.decode(token,key.secret,);
+            let decoded = jwt.decode(token,key.secret);
         console.log(decoded);
         
         next();
