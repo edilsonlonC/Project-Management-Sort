1cc283af80738c8bb6893a00fd41ed452c4dddc4
Edilson Londoño Castañeda <eddylson.londonio@gmail.com>
2019-09-28 01:40:11 -0500

    code async

--- Server/controllers/login.js
+++ Server/controllers/login.js
@@ -3,9 +3,38 @@ const {connection} = require('../Database/DB');
 const bcrypt = require('bcrypt');
 const Keys = require('../config/secret');
 
-let login = (req,res) => {
+let login = async (req,res) => {
     let {email , password} = req.body;
-    connection.query('SELECT Nombre_usuario , Correo , contrasenia FROM Usuarios WHERE Correo = ? ',[email], (err,result) => {
+    if (!email || !password) return res.status({message : 'faltan datos'});
+    try {
+        let queryUser = await connection.query('SELECT Nombre_usuario , Correo , contrasenia , rol FROM Usuarios WHERE Correo = ? ',[email])
+        let userData = queryUser[0];
+        if (userData.length === 0) return res.status(403).send({message : `El usuario con el correo ${email} no existe `})
+        console.log(userData[0]);
+        // payload for token jwt
+        let {Nombre_usuario , Correo ,rol} = userData[0] 
+        let payload = {
+            expire: Date.now() + (1000 * 60 * 60), //1 hour
+            username : Nombre_usuario,
+            email : Correo
+
+        }
+        bcrypt.compare(password,userData[0].contrasenia,(err,resp)=> {
+            if (err) return res.status(500).send({message : `Error : ${err}`});
+            if (!resp) return res.status(403).send({mesage : ' Acceso denegado'});
+            let token = jwt.encode(payload,Keys.secret);
+            return res.status(200).send({ token , user : {
+                Nombre_usuario,
+                rol,
+                Correo
+            } })
+                                           
+        });
+        //return res.status(200).send({userData})
+    } catch (error) {
+        return res.status(500).send({error})
+    }
+    /* connection.query('SELECT Nombre_usuario , Correo , contrasenia FROM Usuarios WHERE Correo = ? ',[email], (err,result) => {
         if (err) return res.status(500).send({message : err});
         if (result < 1) return res.status(403).send({message : 'Acceso denegado'});
         bcrypt.compare(password,result[0].contrasenia,(err,resp)=> {
@@ -15,7 +44,7 @@ let login = (req,res) => {
             return res.status(200).send({message : token});
         });
         
-    });
+    }); */
 
 }
 
--- Server/controllers/project.js
+++ Server/controllers/project.js
@@ -1,58 +1,54 @@
-const {connection} = require('../Database/DB');
+const { connection } = require('../Database/DB');
 
 
 /// modificar, validar primero si el usuario existe
-let saveProject = (req,res) => {
-    let {projectName} = req.body;
+let saveProject = async (req, res) => {
+    let { id } = req.params;
+    let { projectName, type } = req.body;
+
+    if (!projectName || !type) return res.status(404).send({ message: 'Datos incompletos' })
+    try {
+        let queryUser = await connection.query('SELECT Correo FROM Usuarios WHERE id_usuarios = ?', [id])
+        let user = queryUser[0];
+        if (user.length === 0) return res.status(404).send({ message: 'El usuaro no existe ' })
+        let queryProjectSaved = await connection.query('INSERT INTO Proyecto (Nombre_Proyecto,Tipo_Proyecto_id_Tipo,Usuarios_id_usuarios ) values (?,?,?)', [projectName, type, id])
+        return res.status(200).send({ queryProjectSaved });
+    } catch (error) {
+        return res.status(500).send({ error })
+    }
+}
+let projects = async (req, res) => {
+    try {
+        let projectsQuery = await connection.query('SELECT * FROM Proyecto')
+        let projects = projectsQuery[0];
+        if (projects.length === 0) return res.status(404).send({ message: 'no existen proyectos' });
+        return res.status(200).send({ projects });
+    } catch (error) {
+        return res.status(500).send({ error })
+
+    }
+
+}
+
+let updateProject = async (req, res) => {
     let {id} = req.params;
-    connection.query('SELECT id_usuarios FROM Usuarios WHERE id_usuarios = ? ',[id],(err0,Users) => {
-        if (err0) return res.status(500).send({message : `error ${err0}`});
-        if (Users.length < 1) return res.status(404).send({message : 'El usuario no existe'});
-        if (!projectName) return res.status(404).send({message : 'parametros invalidos'});
-        connection.query('SELECT Nombre_Proyecto FROM Proyecto WHERE Nombre_Proyecto = ? ' ,[projectName], (err,result)=> {  
-            if (err) return res.status(500).send({ err});
-            if (result.length >= 1) return res.status(404).send({message : `El proyecto ${projectName} ya existe`});
-            connection.query('INSERT INTO Proyecto (Nombre_Proyecto) values(?)',[projectName],(err2,result2)=>{
-                if (err) return res.status(500).send({message : err2})
-                let {insertId} = result2
-                console.log('Proyecto id',insertId);
-                console.log('User id',id);
-                
-                connection.query('UPDATE Usuarios SET Proyecto_id_proyecto = ? WHERE id_usuarios = ? and Proyecto_id_proyecto = NUll' ,[insertId,id],(err3,success) => {
-                    if (err) return res.status(500).send({message : `Error ${err3}`});
-                    return res.status(200).send({success});
-                })
-                //return res.status(200).send({message : result2});
-            })
-            
-        })
-    })
-   /*  if (!projectName) return res.status(404).send({message : 'parametros invalidos'});
-    connection.query('SELECT Nombre_Proyecto FROM Proyecto WHERE Nombre_Proyecto = ? ' ,[projectName], (err,result)=> {  
-        if (err) return res.status(500).send({ err});
-        if (result.length >= 1) return res.status(404).send({message : `El proyecto ${projectName} ya existe`});
-        connection.query('INSERT INTO Proyecto (Nombre_Proyecto) values(?)',[projectName],(err2,result2)=>{
-            if (err) return res.status(500).send({message : err2})
-            connection.query('UPDATE Usuarios SET Proyecto_id_proyecto = ? WHERE id_usuarios = ? ' [result2.id_proyecto,id],(err3,success) => {
-                if (err) return res.status(500).send({message : `Error ${err3}`});
-                return res.status(200).send({success});
-            })
-            //return res.status(200).send({message : result2});
-        })
-        
-    }) */
-    
-}
-let projects = (req,res) => {
-    res.status(200).send({message : 'projects here '});
+    let {projectName,type} = req.body;
+    try {
+        let queryUpdateProject = await connection.query('UPDATE Proyecto SET Nombre_Proyecto = ? , Tipo_Proyecto_id_Tipo = ? WHERE id_proyecto = ? ',[projectName,type,id])
+        if (queryUpdateProject[0].affectedRows === 0) return res.status(404).send({message : 'El proyecto no existe '});
+        return res.status(200).send({message : 'El proyecto ha sido modificado '})
+    } catch (error) {
+        return res.status(500).send({ error })
+
+    }
+    //res.status(200).send({ message: 'update project' });
 }
 
-let updateProject = (req,res) => {
-    res.status(200).send({message : 'update project'});
-}
-
-let deleteProject = (req,res) => {
-    res.status(200).send({message : 'delete project'});
+let deleteProject = async (req, res) => {
+    let {id} = req.params;
+    let queryDeleteProject = await connection.query('DELETE FROM Proyecto WHERE id_proyecto = ? ',[id]);
+    if(queryDeleteProject[0].affectedRows === 0) return res.status(404).send({message : 'El proyecto no existe '});
+    return res.status(200).send({message : 'proyecto elminado'});
 }
 
 
