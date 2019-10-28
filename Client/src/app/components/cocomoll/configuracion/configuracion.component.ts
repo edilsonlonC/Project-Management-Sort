0554b59d3ed71cfe9fa364826b73f40f11729d6a
Cristian Patiño <c.patino@utp.edu.co>
2019-10-28 12:36:09 -0500

    COCOMOII

--- Client/src/app/components/cocomoll/configuracion/configuracion.component.ts
+++ Client/src/app/components/cocomoll/configuracion/configuracion.component.ts
@@ -263,43 +263,43 @@ export class ConfiguracionComponent implements OnInit {
   cdfs: Dfs = {
     fs: {
       prec: {
-        muyBajo: 0,
-        bajo: 0,
-        nominal: 0,
-        alto: 0,
-        muyAlto: 0,
+        muyBajo: 6.2,
+        bajo: 4.96,
+        nominal: 3.72,
+        alto: 2.48,
+        muyAlto: 1.24,
         extraAlto: 0
       },
       flex: {
-        muyBajo: 0,
-        bajo: 0,
-        nominal: 0,
-        alto: 0,
-        muyAlto: 0,
+        muyBajo: 5.07,
+        bajo: 4.05,
+        nominal: 3.04,
+        alto: 2.03,
+        muyAlto: 1.01,
         extraAlto: 0
       },
       rels: {
-        muyBajo: 0,
-        bajo: 0,
-        nominal: 0,
-        alto: 0,
-        muyAlto: 0,
+        muyBajo: 7.07,
+        bajo: 5.65,
+        nominal: 4.24,
+        alto: 2.83,
+        muyAlto: 1.41,
         extraAlto: 0
       },
       team: {
-        muyBajo: 0,
-        bajo: 0,
-        nominal: 0,
-        alto: 0,
-        muyAlto: 0,
+        muyBajo: 5.48,
+        bajo: 4.38,
+        nominal: 3.29,
+        alto: 2.19,
+        muyAlto: 1.1,
         extraAlto: 0
       },
       pmat: {
-        muyBajo: 0,
-        bajo: 0,
-        nominal: 0,
-        alto: 0,
-        muyAlto: 0,
+        muyBajo: 7.8,
+        bajo: 6.24,
+        nominal: 4.68,
+        alto: 3.12,
+        muyAlto: 1.56,
         extraAlto: 0
       }
     }
@@ -395,6 +395,115 @@ export class ConfiguracionComponent implements OnInit {
     this.updatePS('0', false);
   }
 
+  onActualizar0() {
+    this.configuracion0.get('precmb').setValue(this.dfs.fs.prec.muyBajo);
+    this.configuracion0.get('precb').setValue(this.dfs.fs.prec.bajo);
+    this.configuracion0.get('precn').setValue(this.dfs.fs.prec.nominal);
+    this.configuracion0.get('preca').setValue(this.dfs.fs.prec.alto);
+    this.configuracion0.get('precma').setValue(this.dfs.fs.prec.muyAlto);
+    this.configuracion0.get('precea').setValue(this.dfs.fs.flex.extraAlto);
+    this.configuracion0.get('flexmb').setValue(this.dfs.fs.flex.muyBajo);
+    this.configuracion0.get('flexb').setValue(this.dfs.fs.flex.bajo);
+    this.configuracion0.get('flexn').setValue(this.dfs.fs.flex.nominal);
+    this.configuracion0.get('flexa').setValue(this.dfs.fs.flex.alto);
+    this.configuracion0.get('flexma').setValue(this.dfs.fs.flex.muyAlto);
+    this.configuracion0.get('flexea').setValue(this.dfs.fs.flex.extraAlto);
+    this.configuracion0.get('relsmb').setValue(this.dfs.fs.rels.muyBajo);
+    this.configuracion0.get('relsb').setValue(this.dfs.fs.rels.bajo);
+    this.configuracion0.get('relsn').setValue(this.dfs.fs.rels.nominal);
+    this.configuracion0.get('relsa').setValue(this.dfs.fs.rels.alto);
+    this.configuracion0.get('relsma').setValue(this.dfs.fs.rels.muyAlto);
+    this.configuracion0.get('relsea').setValue(this.dfs.fs.rels.extraAlto);
+    this.configuracion0.get('teammb').setValue(this.dfs.fs.team.muyBajo);
+    this.configuracion0.get('teamb').setValue(this.dfs.fs.team.bajo);
+    this.configuracion0.get('teamn').setValue(this.dfs.fs.team.nominal);
+    this.configuracion0.get('teama').setValue(this.dfs.fs.team.alto);
+    this.configuracion0.get('teamma').setValue(this.dfs.fs.team.muyAlto);
+    this.configuracion0.get('teamea').setValue(this.dfs.fs.team.extraAlto);
+    this.configuracion0.get('pmatmb').setValue(this.dfs.fs.pmat.muyBajo);
+    this.configuracion0.get('pmatb').setValue(this.dfs.fs.pmat.bajo);
+    this.configuracion0.get('pmatn').setValue(this.dfs.fs.pmat.nominal);
+    this.configuracion0.get('pmata').setValue(this.dfs.fs.pmat.alto);
+    this.configuracion0.get('pmatma').setValue(this.dfs.fs.pmat.muyAlto);
+    this.configuracion0.get('pmatea').setValue(this.dfs.fs.pmat.extraAlto);
+  }
+
+  onForm0() {
+    this.dfs.fs.prec.muyBajo =  this.configuracion0.get('precmb').value;
+    this.dfs.fs.prec.bajo =  this.configuracion0.get('precb').value;
+    this.dfs.fs.prec.nominal =  this.configuracion0.get('precn').value;
+    this.dfs.fs.prec.alto =  this.configuracion0.get('preca').value;
+    this.dfs.fs.prec.muyAlto =  this.configuracion0.get('precma').value;
+    this.dfs.fs.prec.extraAlto =  this.configuracion0.get('precea').value;
+    this.dfs.fs.flex.muyBajo =  this.configuracion0.get('flexmb').value;
+    this.dfs.fs.flex.bajo =  this.configuracion0.get('flexb').value;
+    this.dfs.fs.flex.nominal =  this.configuracion0.get('flexn').value;
+    this.dfs.fs.flex.alto =  this.configuracion0.get('flexa').value;
+    this.dfs.fs.flex.muyAlto =  this.configuracion0.get('flexma').value;
+    this.dfs.fs.flex.extraAlto =  this.configuracion0.get('flexea').value;
+    this.dfs.fs.rels.muyBajo =  this.configuracion0.get('relsmb').value;
+    this.dfs.fs.rels.bajo =  this.configuracion0.get('relsb').value;
+    this.dfs.fs.rels.nominal =  this.configuracion0.get('relsn').value;
+    this.dfs.fs.rels.alto =  this.configuracion0.get('relsa').value;
+    this.dfs.fs.rels.muyAlto =  this.configuracion0.get('relsma').value;
+    this.dfs.fs.rels.extraAlto =  this.configuracion0.get('relsea').value;
+    this.dfs.fs.team.muyBajo =  this.configuracion0.get('teammb').value;
+    this.dfs.fs.team.bajo =  this.configuracion0.get('teamb').value;
+    this.dfs.fs.team.nominal =  this.configuracion0.get('teamn').value;
+    this.dfs.fs.team.alto =  this.configuracion0.get('teama').value;
+    this.dfs.fs.team.muyAlto =  this.configuracion0.get('teamma').value;
+    this.dfs.fs.team.extraAlto =  this.configuracion0.get('teamea').value;
+    this.dfs.fs.pmat.muyBajo =  this.configuracion0.get('pmatmb').value;
+    this.dfs.fs.pmat.bajo =  this.configuracion0.get('pmatb').value;
+    this.dfs.fs.pmat.nominal =  this.configuracion0.get('pmatn').value;
+    this.dfs.fs.pmat.alto =  this.configuracion0.get('pmata').value;
+    this.dfs.fs.pmat.muyAlto =  this.configuracion0.get('pmatma').value;
+    this.dfs.fs.pmat.extraAlto =  this.configuracion0.get('pmatea').value;
+  }
+
+  onUpdateForm0() {
+    this.onForm0();
+    this.updateFS('1', true);
+  }
+
+  onFormR0() {
+    this.dfs.fs.prec.muyBajo =  this.cdfs.fs.prec.muyBajo;
+    this.dfs.fs.prec.bajo =  this.cdfs.fs.prec.bajo;
+    this.dfs.fs.prec.nominal =  this.cdfs.fs.prec.nominal;
+    this.dfs.fs.prec.alto =  this.cdfs.fs.prec.alto;
+    this.dfs.fs.prec.muyAlto =  this.cdfs.fs.prec.muyAlto;
+    this.dfs.fs.prec.extraAlto =  this.cdfs.fs.prec.extraAlto;
+    this.dfs.fs.flex.muyBajo =  this.cdfs.fs.flex.muyBajo;
+    this.dfs.fs.flex.bajo =  this.cdfs.fs.flex.bajo;
+    this.dfs.fs.flex.nominal =  this.cdfs.fs.flex.nominal;
+    this.dfs.fs.flex.alto =  this.cdfs.fs.flex.alto;
+    this.dfs.fs.flex.muyAlto =  this.cdfs.fs.flex.muyAlto;
+    this.dfs.fs.flex.extraAlto =  this.cdfs.fs.flex.extraAlto;
+    this.dfs.fs.rels.muyBajo =  this.cdfs.fs.rels.muyBajo;
+    this.dfs.fs.rels.bajo =  this.cdfs.fs.rels.bajo;
+    this.dfs.fs.rels.nominal =  this.cdfs.fs.rels.nominal;
+    this.dfs.fs.rels.alto =  this.cdfs.fs.rels.alto;
+    this.dfs.fs.rels.muyAlto =  this.cdfs.fs.rels.muyAlto;
+    this.dfs.fs.rels.extraAlto =  this.cdfs.fs.rels.extraAlto;
+    this.dfs.fs.team.muyBajo =  this.cdfs.fs.team.muyBajo;
+    this.dfs.fs.team.bajo =  this.cdfs.fs.team.bajo;
+    this.dfs.fs.team.nominal =  this.cdfs.fs.team.nominal;
+    this.dfs.fs.team.alto =  this.cdfs.fs.team.alto;
+    this.dfs.fs.team.muyAlto =  this.cdfs.fs.team.muyAlto;
+    this.dfs.fs.team.extraAlto =  this.cdfs.fs.team.extraAlto;
+    this.dfs.fs.pmat.muyBajo =  this.cdfs.fs.pmat.muyBajo;
+    this.dfs.fs.pmat.bajo =  this.cdfs.fs.pmat.bajo;
+    this.dfs.fs.pmat.nominal =  this.cdfs.fs.pmat.nominal;
+    this.dfs.fs.pmat.alto =  this.cdfs.fs.pmat.alto;
+    this.dfs.fs.pmat.muyAlto =  this.cdfs.fs.pmat.muyAlto;
+    this.dfs.fs.pmat.extraAlto =  this.cdfs.fs.pmat.extraAlto;
+  }
+
+  onRestablecerForm0() {
+    this.onFormR0();
+    this.updateFS('1', false);
+  }
+
   getPS(id: string) {
     this.cocomollService.getJSON(id).subscribe(
       res => {
@@ -411,6 +520,7 @@ export class ConfiguracionComponent implements OnInit {
       res => {
         this.dfs = res;
         console.log(this.dfs);
+        this.onActualizar0();
       },
       err => console.log(err)
     );
@@ -448,31 +558,37 @@ export class ConfiguracionComponent implements OnInit {
     );
   }
 
-  updateFS(id: string) {
+  updateFS(id: string, bandera: boolean) {
     this.cocomollService.updateJSON(id, this.dfs).subscribe(
       res => {
-        console.log(this.dfs);
-        this.getPS('1');
+        if (bandera) {
+          alert('Configuracion Exitosa');
+        }
+        this.getFS('1');
       },
       err => console.log(err)
     );
   }
 
-  updateFCDI(id: string) {
+  updateFCDI(id: string, bandera: boolean) {
     this.cocomollService.updateJSON(id, this.dfcdi).subscribe(
       res => {
-        console.log(this.dfcdi);
-        this.getPS('2');
+        if (bandera) {
+          alert('Configuracion Exitosa');
+        }
+        this.getFCDI('2');
       },
       err => console.log(err)
     );
   }
 
-  updateFCAP(id: string) {
+  updateFCAP(id: string, bandera: boolean) {
     this.cocomollService.updateJSON(id, this.dfcap).subscribe(
       res => {
-        console.log(this.dfcap);
-        this.getPS('3');
+        if (bandera) {
+          alert('Configuracion Exitosa');
+        }
+        this.getFCAP('3');
       },
       err => console.log(err)
     );
